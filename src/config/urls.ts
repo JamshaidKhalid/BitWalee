06cfd46d3098f923d4bd16e49b5d64f8f6dea9b9
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2024-07-23 14:01:08 +0500

    url routing

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -3,11 +3,11 @@
 const NODE_ENV = import.meta.env.VITE_NODE_ENV;
 
 const DEV_API_URL = import.meta.env.VITE_DEV_API_URL || 'http://localhost:3000';
-const PROD_API_URL = import.meta.env.VITE_PROD_API_URL || 'http://bitwaly-server.vercel.app';
+const PROD_API_URL = import.meta.env.VITE_PROD_API_URL || 'http://16.171.201.244:5000';
 const API_URL = NODE_ENV === 'development' ? DEV_API_URL : PROD_API_URL;
 
 const DEV_REDIRECT_URL = import.meta.env.VITE_DEV_REDIRECTION_URL || 'http://localhost:8080';
-const PROD_REDIRECT_URL = import.meta.env.VITE_PROD_REDIRECTION_URL || 'http://bitwaly.vercel.app';
+const PROD_REDIRECT_URL = import.meta.env.VITE_PROD_REDIRECTION_URL || 'http://16.171.201.244:8080'
 const REDIRECT_URL = NODE_ENV === 'development' ? DEV_REDIRECT_URL : PROD_REDIRECT_URL;
 
 export {
